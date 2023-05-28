4536b2cc2b65fd7c1035cd5dc9318f9c3ad1939f
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-05-29 00:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function updateTextWithDelay() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+// Example usage
+const outputElement = document.getElementById('output');
+
+updateTextWithDelay()
+  .then((text) => {
+    outputElement.textContent = text;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
